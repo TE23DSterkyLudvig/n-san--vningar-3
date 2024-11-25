9b8a86aec934c34aa1b46db8409ecdd193a5290b
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-25 09:37:55 +0100

    klar

--- övning 2/övning 2/Program.cs
+++ övning 2/övning 2/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿System.Console.WriteLine("Skriv 1 tal som ska kvadreras");
+String kvaddeboi = Console.ReadLine();
+int.TryParse(kvaddeboi, out int kvadde);
+static void kvadrat(int kvadde){
+    System.Console.WriteLine(kvadde);
+}
+kvadrat(kvadde*kvadde);
+Console.ReadLine();
+
