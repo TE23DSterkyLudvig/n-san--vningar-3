e52e52b01f8c6dd27a7c0f05fe2422df6cf582b3
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-29 10:17:46 +0100

    klar

--- övning 2/övning 2/Program.cs
+++ övning 2/övning 2/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System.Diagnostics;
 
 System.Console.WriteLine("Skriv 1 tal som ska kvadreras");
-String kvaddeboi = Console.ReadLine();
-int.TryParse(kvaddeboi, out int kvadde);
+String kvaddeBoi = Console.ReadLine();
+int.TryParse(kvaddeBoi, out int kvadde);
 static void kvadrat(int kvadde){
-    System.Console.WriteLine(kvadde * kvadde);
+    System.Console.WriteLine(kvadde*kvadde);
 }
 kvadrat(kvadde);
 
@@ -14,12 +14,26 @@ System.Console.WriteLine("Skriv in första talet.");
  System.Console.WriteLine("Skriv in andra talet");
  string Tal2 = Console.ReadLine();
 
- int.TryParse(Tal1, out int Tal1num);
- int.TryParse(Tal2, out int Tal2num);
+ int.TryParse(Tal1, out int tal1Num);
+ int.TryParse(Tal2, out int tal2Num);
 
 static void doub(double tal1, double tal2){
  System.Console.WriteLine( tal1*tal2);
 }
-doub(Tal1num, Tal2num);
+doub(tal1Num, tal2Num);
+
+
+System.Console.WriteLine("Vad är höjden?");
+string height = Console.ReadLine();
+System.Console.WriteLine("Vad är längden?");
+string lenght = Console.ReadLine();
+int.TryParse(height, out int heightNum);
+int.TryParse(lenght, out int lenghtNum);
+
+static void triangleArea(double Height, double Lenght){
+    System.Console.WriteLine((Height*Lenght)/2);
+}
+triangleArea(heightNum,lenghtNum);
 Console.ReadLine();
+
 
