afbda7365f8bc03be590037fe557f29b6a865344
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-11-29 10:00:51 +0100

    klar

--- övning 2/övning 2/Program.cs
+++ övning 2/övning 2/Program.cs
@@ -1,13 +1,25 @@
-﻿System.Console.WriteLine("Skriv 1 tal som ska kvadreras");
+﻿using System.Diagnostics;
+
+System.Console.WriteLine("Skriv 1 tal som ska kvadreras");
 String kvaddeboi = Console.ReadLine();
 int.TryParse(kvaddeboi, out int kvadde);
 static void kvadrat(int kvadde){
     System.Console.WriteLine(kvadde * kvadde);
 }
 kvadrat(kvadde);
-Console.ReadLine();
+
+
+System.Console.WriteLine("Skriv in första talet.");
+ string Tal1 = Console.ReadLine();
+ System.Console.WriteLine("Skriv in andra talet");
+ string Tal2 = Console.ReadLine();
+
+ int.TryParse(Tal1, out int Tal1num);
+ int.TryParse(Tal2, out int Tal2num);
 
 static void doub(double tal1, double tal2){
-
+ System.Console.WriteLine( tal1*tal2);
 }
+doub(Tal1num, Tal2num);
+Console.ReadLine();
 
