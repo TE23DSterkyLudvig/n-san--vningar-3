38b543ef238e010024ab47841ebaa49d45517329
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-12-02 09:54:41 +0100

    fortsätter sen

---
+++ övning 4/övning 4/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+
+System.Console.WriteLine("Skriv 1 tal som ska kvadreras");
+String kvaddeBoi = Console.ReadLine();
+int.TryParse(kvaddeBoi, out int kvadde);
+static int kvadrat(int kvadde)
+{
+    return kvadde * kvadde; 
+}
+int c =kvadrat(kvadde);
+System.Console.WriteLine(c);
+
+
+System.Console.WriteLine("Skriv in första talet.");
+string Tal1 = Console.ReadLine();
+System.Console.WriteLine("Skriv in andra talet");
+string Tal2 = Console.ReadLine();
+
+int.TryParse(Tal1, out int tal1Num);
+int.TryParse(Tal2, out int tal2Num);
+
+static double doub(double tal1, double tal2)
+{
+    return tal1 * tal2;
+    
+}
+double b = doub(tal1Num, tal2Num);
+System.Console.WriteLine(b);
+
+
+System.Console.WriteLine("Vad är höjden?");
+string height = Console.ReadLine();
+System.Console.WriteLine("Vad är längden?");
+string lenght = Console.ReadLine();
+int.TryParse(height, out int heightNum);
+int.TryParse(lenght, out int lenghtNum);
+
+static double triangleArea(double Height, double Lenght)
+{
+    return (Height * Lenght) / 2;
+
+}
+double a = triangleArea(heightNum, lenghtNum);
+Console.WriteLine(a);
+
+//System.Console.WriteLine("Skriv 1 tal");
+//string talet = Console.ReadLine();
+//int.TryParse(talet, out int taletNum);
+  
+    string talet;
+    int taletNum;
+
+  while(true){
+    System.Console.WriteLine("Skriv in ett tal");
+     talet = Console.ReadLine();
+    if(int.TryParse(talet, out  taletNum)){
+         int.TryParse(talet, out  taletNum);
+        break;
+    }
+    else{
+        System.Console.WriteLine("Skriv in igen");
+        Thread.Sleep(800);
+    }
+  }
+
+  static int getNumberinput(int TaletNum, string Talet){
+  while(true){
+    if ( int.TryParse(Talet, out TaletNum)){
+        int.TryParse(Talet, out TaletNum);
+        return TaletNum;
+    }
+    else{
+        System.Console.WriteLine("Skriv in igen");
+    }
+  }
+}
+int e = getNumberinput(taletNum, talet);
+System.Console.WriteLine(e);
+Console.ReadLine();
